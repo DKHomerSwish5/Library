a38b4e94457569f8ee7ed29a49951cd8440894de
Joe <csp.x520@gmail.com>
2023-09-06 19:52:00 -0400

    Have Modal Book Form Inputs

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const addBtn = document.querySelector('.add-btn');
+const modal = document.getElementById("myModal");
 const myLibrary = [];
 
+
+addBtn.addEventListener("click", bookModal);
+
+function bookModal(){
+    modal.style.display = "block";
+}
 function Book(){
 
 }

4270e0adc2a078c8db95c1cdc21bd9b31c40ac6d
Joe <csp.x520@gmail.com>
2023-09-07 19:55:16 -0400

    Have Book Input Modal Box

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ const myLibrary = [];
 
 
 addBtn.addEventListener("click", bookModal);
-
+window.addEventListener("click", closeModal);
 function bookModal(){
     modal.style.display = "block";
+}
+function closeModal(event){
+    if (event.target == modal) {
+        modal.style.display = "none";
+      }
 }
 function Book(){
 

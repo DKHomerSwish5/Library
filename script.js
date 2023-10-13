718ab5d3e42b39780e793af2cbe78e2a29a51bc4
Joe <csp.x520@gmail.com>
2023-10-12 20:28:17 -0400

    Add user input card and update read and remove buttons on cards

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const addBtn = document.querySelector('.add-btn');
-const remBtn = document.querySelector('.remove-btn');
 const submitBtn = document.querySelector('.submit-btn');
 const modal = document.getElementById("myModal");
 const form = document.getElementById("input-form");
 
 let myLibrary = [];
 addBtn.addEventListener("click", bookModal);
-remBtn.addEventListener("click", deleteBook);
 submitBtn.addEventListener("click", addBookToLibrary);
 window.addEventListener("click", closeModal);
+
 function bookModal(){
     modal.style.display = "block";
 }
@@ -17,9 +16,6 @@ function closeModal(event){
         modal.style.display = "none";
     }
     return
-}
-function deleteBook(event){
-    
 }
 function Book(title, author, pages, read){
     this.title = title,
@@ -36,51 +32,42 @@ function addBookToLibrary(event){
     let read = document.getElementById("Checkbox");
     let addedBook = new Book(title.value, author.value, pages.value, read.checked);
     myLibrary.push(addedBook);
-    createCard(addedBook);
-    console.log(myLibrary);
     modal.style.display = "none";
+    myLibrary.forEach(book => {
+        const cardDiv = document.createElement('div');
+        cardDiv.className = "card";
+        const titleP = document.createElement('p');
+        titleP.className = "title";
+        titleP.textContent = `${book.title}`;
+        const authorP = document.createElement('p');
+        authorP.className = "author";
+        authorP.textContent = `${book.author}`;
+        const pagesP = document.createElement('p');
+        pagesP.className = "pages";
+        pagesP.textContent = `${book.pages}`;
+        const readBtn = document.createElement('button');
+        readBtn.className = "read-btn";
+        readBtn.textContent = "Read";
+        const removeBtn = document.createElement('button');
+        removeBtn.className = "remove-btn";
+        removeBtn.textContent = "Remove";
+    
+        const parentDiv = document.querySelector('.card-grid');
+        parentDiv.appendChild(cardDiv);
+        cardDiv.appendChild(titleP);
+        cardDiv.appendChild(authorP);
+        cardDiv.appendChild(pagesP);
+        cardDiv.appendChild(readBtn);
+        cardDiv.appendChild(removeBtn);
+        removeBtn.addEventListener("click", ()=> {
+            parentDiv.removeChild(cardDiv);
+            myLibrary.splice(myLibrary.indexOf(book), 1);
+        })
+        readBtn.addEventListener("click", ()=>{
+            readBtn.style.backgroundColor = "yellow";
+            readBtn.textContent = "Not Read";
+        } )
+    });
     form.reset();
-    return
-}
-function createCard(addedBook){
-    const cardDiv = document.createElement('div');
-    cardDiv.className = "card";
-    const titleP = document.createElement('p');
-    titleP.className = "title";
-    const authorP = document.createElement('p');
-    authorP.className = "author";
-    const pagesP = document.createElement('p');
-    pagesP.className = "pages";
-    const readBtn = document.createElement('button');
-    readBtn.className = "read-btn";
-    readBtn.textContent = "Read";
-    const removeBtn = document.createElement('button');
-    removeBtn.className = "remove-btn";
-    removeBtn.textContent = "Remove";
-    
-    for (let key in addedBook){
-        if(addedBook.hasOwnProperty(key)){
-            const value = addedBook[key];
-            if(key === 'title'){
-                titleP.textContent = `${value}`;
-            }
-            else if(key === 'author'){
-                authorP.textContent = `${value}`;
-            }
-            else if(key === 'pages'){
-                pagesP.textContent = `${value}`;
-            }
-        }
-    }
-
-    
-    const parentDiv = document.querySelector('.card-grid');
-    parentDiv.appendChild(cardDiv);
-    cardDiv.appendChild(titleP);
-    cardDiv.appendChild(authorP);
-    cardDiv.appendChild(pagesP);
-    cardDiv.appendChild(readBtn);
-    cardDiv.appendChild(removeBtn);
-
     return
 }
